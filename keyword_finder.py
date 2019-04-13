026e97871bf8e1c0a1dc9c2c8765940b33e83fa9
Aizmik <blek_1990@list.ru>
2019-04-13 08:24:18 +0300

    Added keyword finding: city and time

---
+++ keyword_finder.py
@@ -0,0 +1,36 @@
+import nltk
+import pymorphy2
+
+def find_city(sentence):  
+    morph = pymorphy2.MorphAnalyzer()
+    cities = []
+
+    for word in nltk.word_tokenize(sentence):
+        tags = str(morph.parse(word)[0].tag)
+
+        if 'Geox sing' in tags:
+            cities.append(word)
+
+    return cities
+
+
+def find_day(sentence):
+    morph = pymorphy2.MorphAnalyzer()
+    possible_tags = ['ADVB', 'NOUN,inan,femn sing,accs',
+     'NOUN,inan,masc sing,accs']
+    times = []
+
+    for word in nltk.word_tokenize(sentence):
+            tags = str(morph.parse(word)[0].tag)
+
+            if tags in possible_tags:
+                times.append(word)
+
+    return times
+
+
+with open('sample.txt', 'r') as f:
+    for line in f.readlines():
+        print(find_day(line))
+
+
