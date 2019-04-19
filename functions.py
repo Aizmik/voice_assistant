5b84c7cfa35bf91c994d1791b7d40aba251962ef
Aizmik <blek_1990@list.ru>
2019-04-19 06:49:14 +0300

    Improved output

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ import datetime
 import bs4
 
 
-def get_weather(when=None, location='СанктПетербурге'):
+def get_weather(when=['сейчас'], location=['СанктПетербурге']):
     """ when should be normalized
     """
     dow = {'Понедельник': 'Пн', 'Вторник': 'Вт', 'Среда': 'Ср', 'Четверг': 'Чт',
@@ -19,20 +19,29 @@ def get_weather(when=None, location='СанктПетербурге'):
     page = r.urlopen(url)
     soup = bs4.BeautifulSoup(page, 'html.parser')
 
-    if when:
+    if not when == ['сейчас']:
         for forecast in soup.select('a.link'):
             try:
                 cast = bs4.BeautifulSoup(str(forecast), 'html.parser')
-                if cast.select('div.forecast-briefly__name')[0].text == dow[when]:
-                    return[cast.select('div.temp > span.temp__value')[0].text,
-                           cast.select('div.forecast-briefly__condition')[0].text]
+                day = cast.select('div.forecast-briefly__name')[0].text
+                if day == dow[when]:
+                    temp = cast.select('div.temp > span.temp__value')[0].text
+                    condition = cast.select('div.forecast-briefly__condition')[0].text
+
+                    return 'В ' + str(location[0]) + ' ' + str(when)\
+                           + ', ' + temp + ', ' + condition
             except(Exception):
                 pass
 
-    return [soup.select('span.temp__value')[0].text,
-            soup.select('div.link__condition')[0].text,
-            soup.select('div.temp > span.temp__value')[1].text]
+    when = ['сейчас']
+    if not location:
+        location = ['СанктПетербурге']
+    return 'В ' + str(location[0]) + ' ' + str(when[0])\
+           + ' ' + str(soup.select('span.temp__value')[0].text)\
+           + ', ' + str(soup.select('div.link__condition')[0].text)\
+           + ', ' + 'ощущается' + ' ' + 'как' + ' ' + str(soup.select(
+                'div.temp > span.temp__value')[1].text)
 
 
 def get_time():
-    return datetime.datetime.now()
+    return datetime.datetime.now().time()
